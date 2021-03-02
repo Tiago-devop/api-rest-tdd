8b85a767514587385a56ab4af0ac9ed20a9a6c26
Tiago-devop <tiagodev@protonmail.com>
2021-03-02 16:23:39 -0300

    chore: add app

--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-const supertest = require("supertest");
-
-const request = supertest("http://localhost:4001");
-
-test("Deve responder na porta 4001", () => {
-  // acessar a url http://localhost:4001
-  return request.get("/").then((res) => expect(res.status).toBe(200));
-  // verificar que a resposta foi 200
-});
