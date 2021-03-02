59d57b34a44d70bc915b611cf2a7c50351a9e205
Tiago-devop <tiagodev@protonmail.com>
2021-03-02 17:05:29 -0300

    chore: must list users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ app.get("/", (req, res) => {
   res.status(200).send();
 });
 
+app.get("/users", (req, res) => {
+  const users = [{ name: "Tiago Doe", mail: "tiago@mail.com" }];
+  res.status(200).json(users);
+});
+
 module.exports = app;
